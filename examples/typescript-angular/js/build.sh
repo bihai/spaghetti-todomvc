5caa793926325874970d1f421ec03cf26b01549c
Balazs Dano <balazs.dano@prezi.com>
2014-11-07 12:44:11 +0100

    We have ts-ts version

--- examples/typescript-angular/js/build.sh
+++ examples/typescript-angular/js/build.sh
@@ -7,3 +7,8 @@ cd ui
 ./build.sh
 cd ../
 spaghetti package --wrapper requirejs --dependency-path storage/build/bundle:ui/build/bundle --execute --main todos.ui --output build/app
+
+mkdir build/webapp
+cp -r build/app/* build/webapp
+cp -r ../bower_components build/webapp/bower_components
+cp ../index.html build/webapp
